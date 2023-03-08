1269c81c81a120909db5f43f33d77e853063c674
mahmoud sabra <mahmoudsabraa25@gmail.com>
2023-03-08 02:05:54 +0200

    modification of some resources

--- NATGW.tf
+++ NATGW.tf
@@ -1,20 +1,20 @@
-# resource "aws_eip" "nat-eip" {
-#   count = var.preferred_number_of_elastic_ip
-#   depends_on = [
-#     aws_internet_gateway.gw
-#   ]
-#   vpc = true
-#   tags = {
-#     "name" = format("nat-eip-%d",count.index)
-#   }
-# }
-# resource "aws_nat_gateway" "natgw" {
-#   count = var.preferred_number_of_natgw
-#   allocation_id = aws_eip.nat-eip[count.index].id
-#   subnet_id     = element(aws_subnet.public.*.id, count.index)
+resource "aws_eip" "nat-eip" {
+  count = var.preferred_number_of_elastic_ip
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+  vpc = true
+  tags = {
+    "name" = format("nat-eip-%d",count.index)
+  }
+}
+resource "aws_nat_gateway" "natgw" {
+  count = var.preferred_number_of_natgw
+  allocation_id = aws_eip.nat-eip[count.index].id
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
 
-#   tags = {
-#     "Name" = format("natgw-%d",count.index)
-#   }
-#   depends_on = [aws_internet_gateway.gw]
-# }
+  tags = {
+    "Name" = format("natgw-%d",count.index)
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
--- SNS.tf
+++ SNS.tf
@@ -1,20 +1,20 @@
-# # creating sns topic for all the auto scaling groups
-# resource "aws_sns_topic" "iac-sns" {
-#   name = "Default_CloudWatch_Alarms_Topic"
-# }
+# creating sns topic for all the auto scaling groups
+resource "aws_sns_topic" "iac-sns" {
+  name = "Default_CloudWatch_Alarms_Topic"
+}
 
-# resource "aws_autoscaling_notification" "iac_notifications" {
-#   group_names = [
-#     aws_autoscaling_group.bastion-asg.name,
-#     aws_autoscaling_group.nginx-asg.name,
-#     aws_autoscaling_group.webserver-asg.name,
-#   ]
-#   notifications = [
-#     "autoscaling:EC2_INSTANCE_LAUNCH",
-#     "autoscaling:EC2_INSTANCE_TERMINATE",
-#     "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
-#     "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
-#   ]
+resource "aws_autoscaling_notification" "iac_notifications" {
+  group_names = [
+    aws_autoscaling_group.bastion_asg.name,
+    aws_autoscaling_group.nginx_asg.name,
+    aws_autoscaling_group.webserver_asg.name,
+  ]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
 
-#   topic_arn = aws_sns_topic.iac-sns.arn
-# }
+  topic_arn = aws_sns_topic.iac-sns.arn
+}
